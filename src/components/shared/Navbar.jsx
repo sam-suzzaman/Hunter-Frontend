2da7b4271bd124bc249f7e47e25db918743dec91
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-09 18:27:12 +0600

    add simple navbar

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "../Logo";
+
+const Navbar = () => {
+    return (
+        <Wrapper>
+            <div className="container">
+                <Logo />
+            </div>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    box-shadow: 0 5px 5px var(--shadow-light);
+    padding: 1rem 0;
+    .container {
+        width: 100%;
+        max-width: 1200px;
+    }
+    @media screen and (max-width: 1200px) {
+        padding: 1rem 2rem;
+    }
+    @media screen and (max-width: 500px) {
+        padding: 0.7rem 1rem;
+    }
+`;
+
+export default Navbar;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,36 +2,41 @@ import React from "react";
 import Wrapper from "../assets/css/wrappers/LandingPage";
 import { Link } from "react-router-dom";
 import photo from "../assets/media/LandingPage/hero.png";
+import Navbar from "../components/shared/Navbar";
 
 const Landing = () => {
     return (
-        <Wrapper>
-            <div className="hero-content">
-                <div className="text-content">
-                    <h1>
-                        Biggest <span className="fancy">Job Tracking</span> App
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Illo perferendis dignissimos eligendi voluptas
-                        exercitationem, eius aut mollitia quasi nisi voluptatem
-                        similique, tempore totam, odit repellendus non. Dolores
-                        eos animi recusandae.
-                    </p>
-                    <div className="btn-grp">
-                        <Link className="btn" to="/login">
-                            login
-                        </Link>
-                        <Link className="btn" to="/register">
-                            register
-                        </Link>
+        <>
+            <Navbar />
+            <Wrapper>
+                <div className="hero-content">
+                    <div className="text-content">
+                        <h1>
+                            Biggest <span className="fancy">Job Tracking</span>{" "}
+                            App
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Illo perferendis dignissimos eligendi voluptas
+                            exercitationem, eius aut mollitia quasi nisi
+                            voluptatem similique, tempore totam, odit
+                            repellendus non. Dolores eos animi recusandae.
+                        </p>
+                        <div className="btn-grp">
+                            <Link className="btn" to="/login">
+                                login
+                            </Link>
+                            <Link className="btn" to="/register">
+                                register
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="placeholder">
+                        <img src={photo} alt="job viva photo" />
                     </div>
                 </div>
-                <div className="placeholder">
-                    <img src={photo} alt="job viva photo" />
-                </div>
-            </div>
-        </Wrapper>
+            </Wrapper>
+        </>
     );
 };
 
