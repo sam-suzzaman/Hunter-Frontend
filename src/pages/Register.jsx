3687e44c21274a8687cd6bfad7c738183bcf8a6e
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-10 09:03:13 +0600

    add login,register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,158 @@
 import React from "react";
+import styled from "styled-components";
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return <div>this is register page</div>;
+    return (
+        <Wrapper>
+            <div className="container">
+                <div className="flex justify-center">
+                    <Logo />
+                </div>
+                <h1>Create Account</h1>
+                <form>
+                    <div className="row">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            autoComplete="off"
+                            placeholder="Type Here"
+                        />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            autoComplete="off"
+                            placeholder="Email@example.com"
+                        />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Type Here"
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+                <div className="">
+                    <p className="text-center text-[10px] font-semibold opacity-9 mt-3">
+                        Already have an account.
+                        <Link className="ml-1 link" to="/login">
+                            Login now
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background: #f9faff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 0;
+    .container {
+        background: var(--color-white);
+        max-width: 360px;
+        width: 100%;
+        padding: 58px 44px;
+        border: 1px solid #e1e2f0;
+        border-radius: 4px;
+        box-shadow: 0 0 5px 0 rgba(42, 45, 48, 0.12);
+        transition: all 0.3s ease;
+    }
+    h1 {
+        margin-top: 20px;
+        text-align: center;
+        text-transform: capitalize;
+        font-size: calc(1rem + 0.3vw);
+        font-weight: 600;
+        color: var(--color-primary);
+    }
+    form {
+        margin-top: calc(1rem + 0.9vw);
+    }
+
+    .row {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    .row label {
+        font-size: 12px;
+        color: var(--color-black);
+        font-weight: 400;
+        margin-bottom: 2px;
+    }
+
+    .row input {
+        flex: 1;
+        padding: 8px 10px;
+        border: 1px solid #d6d8e6;
+        border-radius: 4px;
+        font-size: 12px;
+        font-weight: 500;
+        transition: all 0.2s ease-out;
+    }
+
+    .row input:focus {
+        outline: none;
+        box-shadow: inset 2px 2px 5px 0 rgba(42, 45, 48, 0.12);
+    }
+
+    .row input::placeholder {
+        color: var(--color-black);
+        opacity: 0.7;
+    }
+
+    button {
+        width: 50%;
+        min-width: 100px;
+        padding: 8px;
+        font-size: 16px;
+        letter-spacing: 1px;
+        background: var(--color-accent);
+        color: var(--color-white);
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        margin: 15px auto 0;
+        transition: background 0.2s ease-out;
+    }
+
+    button:hover {
+        background: var(--color-primary);
+    }
+
+    @media (max-width: 458px) {
+        .container {
+            width: 90%;
+            padding: 30px 0;
+        }
+        form {
+            padding: 0 20px;
+        }
+    }
+    p .link {
+        text-transform: capitalize;
+        color: var(--color-primary);
+    }
+    p .link:hover {
+        text-decoration: underline;
+    }
+`;
 
 export default Register;
